47c99fb0799c6c2030da176ef567860d99732e46
Sindre Nerdal <sindre.nerdal@capgemini.com>
2018-09-20 12:52:00 +0200

    Added testing spec

--- ConceptsMicroservice.UnitTests/Utilities/FileDBTest.cs
+++ ConceptsMicroservice.UnitTests/Utilities/FileDBTest.cs
@@ -1,9 +1,4 @@
-﻿
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using ConceptsMicroservice.Models;
-using ConceptsMicroservice.Utilities;
+﻿using ConceptsMicroservice.Utilities;
 using Xunit;
 
 namespace ConceptsMicroservice.UnitTests
@@ -12,41 +7,19 @@ namespace ConceptsMicroservice.UnitTests
     {
 
         private IFileDB _db;
-        private string _projectDirectory;
-        private string _filesDirectory;
 
         public FileDBTest()
         {
             _db = new FileDB();
-            _projectDirectory = System.Environment.CurrentDirectory;
-            _filesDirectory = Path.Join(_projectDirectory, "Files");
         }
 
         [Fact]
-        public void ReadFile_Returns_Empty_List_If_File_Is_Empty()
+        public void ReadFile_Returns_Empty_List_If_Resources_Does_Not_Exist()
         {
-            _db.DbFilePath = Path.Join(_filesDirectory, "no_concepts.csv");
+            _db.URL = "fake url";
             var concepts = _db.ReadFromFile();
 
             Assert.Empty(concepts);
-        }
-
-        [Fact]
-        public void ReadFile_Returns_Empty_List_If_File_Does_Not_Exist()
-        {
-            _db.DbFilePath = Path.Join(_filesDirectory, "does not even exist....csv");
-            var concepts = _db.ReadFromFile();
-
-            Assert.Empty(concepts);
-        }
-
-        [Fact]
-        public void ReadFile_Returns_List_Of_ConceptItems_If_File_Exist()
-        {
-            _db.DbFilePath = Path.Join(_filesDirectory, "concepts.csv");
-            var concepts = _db.ReadFromFile();
-
-            Assert.NotEmpty(concepts);
         }
     }
 }
